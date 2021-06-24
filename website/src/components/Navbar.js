815f80ee95dd0a264c067c471c0e0d107492af62
Akilax0 <killerilaksha@gmail.com>
2021-06-24 21:32:51 +0530

    web

---
+++ website/src/components/Navbar.js
@@ -0,0 +1,50 @@
+// import Home from '../pages/Home/Home'
+import './navbar-styles.css'
+
+function Navbar() {
+    return(
+        <body>
+
+        {/* <div className='navbar'>
+            <ul className='nav'>
+                <li><a href='#home'>Home</a></li>
+                <li><a href='#intro'>Introduction</a></li>
+                <li><a href='#ha'>Hardware Architecture</a></li>
+                <li><a href='#hd'>Hardware Design</a></li>
+                <li><a href='#sd'>Software Design</a></li>
+                <li><a href='#budget'>Budget</a></li>
+                <li><a href='#team'>Team</a></li>
+            </ul>
+        </div>
+
+        <div className='banner-area' id="home"></div>
+        <Home/>
+        <div className='intro-area' id='intro'>
+            <h1>Introduction</h1>
+        </div>
+        <hr/>
+        <div className='ha-area' id='ha'>
+            <h1>Hardware Architecture</h1>       
+        </div>
+        <hr/>
+        <div className='hd-area' id='hd'>
+            <h1>Hardware Design</h1>             
+        </div>
+        <hr/>
+        <div className='sd-area' id='sd'>
+            <h1>Software Design</h1>            
+        </div>
+        <hr/>
+        <div className='budget-area' id='budget'>
+            <h1>Budget</h1>         
+        </div>
+        <hr/>
+        <div className='team-area' id='team'>
+            <h1>Team</h1>
+        </div> */}
+        </body>
+        
+    )
+}
+
+export default Navbar;
---
+++ website/src/pages/Home/Home.js
@@ -0,0 +1,10 @@
+import './Home.css'
+
+function Home() {
+    return(
+        <div className="banner-area">
+        </div>
+    )
+}
+
+export default Home;
