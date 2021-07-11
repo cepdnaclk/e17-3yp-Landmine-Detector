47c0e107e9a82d689246068ddb30151db6f46d8d
thisaramanohara <thisaramanohara@gmail.com>
2021-07-11 17:48:06 +0530

    Links added

--- website/src/pages/Team/Team.js
+++ website/src/pages/Team/Team.js
@@ -6,35 +6,47 @@ function Team() {
             <h1 className='main-topic'>Team</h1>
             <div className="team-members">
                 <div className="member">
-                <img className="member-img" alt="" src={process.env.PUBLIC_URL + '/images/akila.jpg'} width="200" height="190" />
+                    <img className="member-img" alt="" src={process.env.PUBLIC_URL + '/images/akila.jpg'} width="200" height="190" />
                     <h3>Akila Karunanayake</h3>
                     <a href="#" target="_blank">
                         <img alt="" src={process.env.PUBLIC_URL + '/png/linkedin.png'} width="40" height="40" />
                         &emsp;
+                    </a>
+                    <a href="#" target="_blank">
                         <img alt="" src={process.env.PUBLIC_URL + '/png/fb.png'} width="40" height="40" />
                         &emsp;
+                    </a>
+                    <a href="#" target="_blank">
                         <img alt="" src={process.env.PUBLIC_URL + '/png/web.png'} width="40" height="40" />
-                    </a> 
+                    </a>
                 </div>
                 <div className="member">
                     <img className="member-img" alt="" src={process.env.PUBLIC_URL + '/images/thisara.jpg'} width="200" height="190" />
                     <h3>Thisara Manohara</h3>
-                    <a href="#" target="_blank">
+                    <a href="https://www.linkedin.com/in/thisara-manohara-6990911a8/" target="_blank">
                         <img alt="" src={process.env.PUBLIC_URL + '/png/linkedin.png'} width="40" height="40" />
                         &emsp;
+                    </a>
+                    <a href="#" target="_blank">
                         <img alt="" src={process.env.PUBLIC_URL + '/png/fb.png'} width="40" height="40" />
                         &emsp;
+                    </a>
+                    <a href="https://www.thisaramanohara.me/" target="_blank">
                         <img alt="" src={process.env.PUBLIC_URL + '/png/web.png'} width="40" height="40" />
-                    </a> 
+                    </a>
                 </div>
                 <div className="member">
                     <img className="member-img" alt="" src={process.env.PUBLIC_URL + '/images/vishva.jpg'} width="200" height="190" />
-                    <h3>Vishva Rupasinghe</h3>
-                    <a href="#" target="_blank">
+                    <h3>Vishva Navanjana</h3>
+                    <a href="https://www.linkedin.com/in/vishva-navanjana-67334116a/" target="_blank">
                         <img alt="" src={process.env.PUBLIC_URL + '/png/linkedin.png'} width="40" height="40" />
                         &emsp;
+                    </a>
+                    <a href="#" target="_blank">
                         <img alt="" src={process.env.PUBLIC_URL + '/png/fb.png'} width="40" height="40" />
                         &emsp;
+                    </a>
+                    <a href="https://www.vishva.me/" target="_blank">
                         <img alt="" src={process.env.PUBLIC_URL + '/png/web.png'} width="40" height="40" />
                     </a> 
                 </div>
@@ -43,26 +55,35 @@ function Team() {
             <h1 className='main-topic'>Supervisors</h1>
             <div className="team-members">
                 <div className="member">
-                <img className="member-img" alt="" src={process.env.PUBLIC_URL + '/images/isuru.png'} width="200" height="190" />
+                    <img className="member-img" alt="" src={process.env.PUBLIC_URL + '/images/isuru.png'} width="200" height="190" />
                     <h3>Dr. Isuru Nawinne</h3>
                     <a href="#" target="_blank">
                         <img alt="" src={process.env.PUBLIC_URL + '/png/linkedin.png'} width="40" height="40" />
                         &emsp;
+                    </a>
+                    <a href="#" target="_blank">
                         <img alt="" src={process.env.PUBLIC_URL + '/png/fb.png'} width="40" height="40" />
                         &emsp;
+                    </a>
+                    <a href="#" target="_blank">
                         <img alt="" src={process.env.PUBLIC_URL + '/png/web.png'} width="40" height="40" />
-                    </a> 
+                    </a>
                 </div>
+                
                 <div className="member">
                     <img className="member-img" alt="" src={process.env.PUBLIC_URL + '/images/mahanama.jpg'} width="200" height="190" />
                     <h3>Dr. Mahanama Wickramasinghe</h3>
-                    <a href="#" target="_blank">
+                    <a href="https://www.linkedin.com/in/mahanama" target="_blank">
                         <img alt="" src={process.env.PUBLIC_URL + '/png/linkedin.png'} width="40" height="40" />
                         &emsp;
+                    </a>
+                    <a href="#" target="_blank">
                         <img alt="" src={process.env.PUBLIC_URL + '/png/fb.png'} width="40" height="40" />
                         &emsp;
+                    </a>
+                    <a href="#" target="_blank">
                         <img alt="" src={process.env.PUBLIC_URL + '/png/web.png'} width="40" height="40" />
-                    </a> 
+                    </a>
                 </div>
             </div>
 
