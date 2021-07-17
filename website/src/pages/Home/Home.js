bb91a2d78666033f1bd3b3e8a86f6e9bb77ab0d1
thisaramanohara <thisaramanohara@gmail.com>
2021-07-17 15:19:21 +0530

    Header added

--- website/src/pages/Home/Home.js
+++ website/src/pages/Home/Home.js
@@ -4,9 +4,13 @@ import TypeWriter from '../../components/TypeWriter'
 
 function Home() {
     return(
-        <div className="banner-area" id={'home'}>
-            <TypeWriter></TypeWriter>
+        <div>
+            <h1 className='heading'>Landmine Detection Robot</h1>
+           <div className="banner-area" id={'home'}>
+            {/* <TypeWriter></TypeWriter> */}
+            </div> 
         </div>
+        
     )
 }
 export default Home;
