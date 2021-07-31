f39d9d3f02b8ef482e86e8f7aaaabd79023052e5
VishvaNavanjana <vishvanavanjana@gmail.com>
2021-07-31 22:32:06 +0530

    add a table and data

--- website/src/pages/Budget/Budget.js
+++ website/src/pages/Budget/Budget.js
@@ -7,6 +7,85 @@ function Budget() {
             <br></br>
             <br></br>
             <br></br>
+            
+            <table>
+    <tr>
+        
+        <th>Component </th>
+        <th>Unit Price(Rs.)</th>
+        <th>Quantity</th>
+        <th>Price</th>
+    </tr>
+    <tr>
+        <td>ESP32</td>
+        <td>1500</td>
+        <td>1</td>
+        <td>1500</td>
+    </tr>
+    <tr>
+        <td>Seeduino</td>
+        <td>1600</td>
+        <td>1</td>
+        <td>1600</td>
+    </tr>
+    <tr>
+        <td>NEO6M-GPS</td>
+        <td>2190</td>
+        <td>1</td>
+        <td>2190</td>
+    </tr>
+    <tr>
+        <td>GSM Module</td>
+        <td>1950</td>
+        <td>1</td>
+        <td>1950</td>
+    </tr>
+    <tr>
+        <td>GYRO/Accelerometer</td>
+        <td>2800</td>
+        <td>1</td>
+        <td>2800</td>
+    </tr>
+    <tr>
+        <td>Motors</td>
+        <td>1500</td>
+        <td>4</td>
+        <td>6000</td>
+    </tr>
+    <tr>
+        <td>"Tires Chassis and other robot components"</td>
+        <td>5000</td>
+        <td>1</td>
+        <td>5000</td>
+    </tr>
+    <tr>
+        <td>Copper wire</td>
+        <td>250</td>
+        <td>1 (10m)</td>
+        <td>250</td>
+    </tr>
+    <tr>
+        <td>"Resistors capacitors and other components"</td>
+        <td>500</td>
+        <td>1</td>
+        <td>500</td>
+    </tr>
+    <tr>
+        <td>12V battery</td>
+        <td>2500</td>
+        <td>1</td>
+        <td>2500</td>
+    </tr>
+    <tr>
+        <td>Total</td>
+        <td></td>
+        <td></td>
+        <td>24350</td>
+    </tr>
+    <tr>
+        <td></td>
+    </tr>
+</table>
 
             
           
