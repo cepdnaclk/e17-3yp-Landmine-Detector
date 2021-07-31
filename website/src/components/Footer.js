5ae69c3b64c870603df9393dee8edfdca53916e3
thisaramanohara <thisaramanohara@gmail.com>
2021-07-31 13:04:54 +0530

    Updated footer

--- website/src/components/Footer.js
+++ website/src/components/Footer.js
@@ -3,11 +3,12 @@ import './Footer.css'
 
 const Footer = () => {
     return(
-
+        
+        <div>
         <div className='row'>
             {/* col1 */}
             <div className='col'>
-                <h1>University of Peradeniya</h1>
+                <h2>University of Peradeniya</h2>
                 <strong>Phone: </strong> &emsp;
                 +94 81 239 33 00 <br/>
                 <p><strong>Web-site:</strong><a href="http://www.pdn.ac.lk/" target="_blank">http://www.pdn.ac.lk/</a></p>
@@ -15,7 +16,7 @@ const Footer = () => {
 
             {/* col2 */}
             <div className='col'>
-                <h1>Faculty of Engineering</h1>
+                <h2>Faculty of Engineering</h2>
                 <strong>Phone: </strong> &emsp;
                 +94 81 239 33 02 <br/>
                 <p><strong>Web-site:</strong><a href="http://eng.pdn.ac.lk/" target="_blank">http://eng.pdn.ac.lk/</a></p>
@@ -23,11 +24,20 @@ const Footer = () => {
 
             {/* col3 */}
             <div className='col'>
-                <h1>Computer Engineering Department</h1>
+                <h2>Computer Engineering Department</h2>
                 <strong>Phone: </strong> &emsp;
                 +94 81 239 39 14 <br/>
                 <p><strong>Web-site:</strong><a href="http://www.ce.pdn.ac.lk/" target="_blank">http://www.ce.pdn.ac.lk/</a></p>
             </div>
+
+        </div>
+
+        <hr/>
+        <div className='row'>
+            <p>
+                &copy; {new Date().getFullYear()} LANDMINE DETECTOR | All right reserved | Terms of Service | Privacy
+            </p>
+        </div>
 
         </div>
 
