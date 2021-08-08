c8f85167ddafcdc93fc3589e53d08a2f8208c56c
Akilax0 <killerilaksha@gmail.com>
2021-08-08 23:41:41 +0530

    visualizer_setup

--- visualizer/src/index.ts
+++ visualizer/src/index.ts
@@ -3,9 +3,9 @@ import { GreaterEqualDepth } from 'three'
 import * as THREE from 'three'
 
 var direction = new THREE.Vector3(1, 0, 0);
-var box1;
 // scalar to simulate speed
 var speed = 1;
+var box1;
 
 export class PhysicsTest extends Scene3D{
 
@@ -26,15 +26,49 @@ export class PhysicsTest extends Scene3D{
     // this.camera
     // this.scene
     // this.physics
+
+    box1.body.setCollisionFlags(2)
+  //   const teleport = () => {
+  //     // set body to be kinematic
+  //     box1.body.setCollisionFlags(2)
+
+  //     // set the new position
+  //     box1.position.set(2, 4, 2)
+  //     box1.body.needUpdate = true
+
+  //     // this will run only on the next update if body.needUpdate = true
+  //     box1.body.once.update(() => {
+  //       // set body back to dynamic
+  //       box1.body.setCollisionFlags(0)
+
+  //       // if you do not reset the velocity and angularVelocity, the object will keep it
+  //       box1.body.setVelocity(0, 0, 0)
+  //       box1.body.setAngularVelocity(0, 0, 0)
+  //     })
+  //   }
+  //   setTimeout(teleport, 2000)
   }
 
-  update(){
-    var vector = direction.clone().multiplyScalar(speed);
-    
-    box1.position.x += vector.x;
-    box1.position.y += vector.y;
-    box1.position.z += vector.z;
-  
+  update(time) {
+
+    const orbitRadius = 5
+    const date = time * 0.0025
+    const { x, y, z } = box1.position.clone()
+
+          // make object orbit around a center
+          // https://stackoverflow.com/questions/42418958/rotate-an-object-around-an-orbit
+          box1.position.set(
+            Math.cos(date) * orbitRadius + 0,
+            0,
+            Math.sin(date) * orbitRadius + 0
+          )
+
+    // box1.position.set(0,5,0)
+    // box1.rotation.x =+ 0.01
+    // set needUpdate to true, every time you want
+    // to adjust the position or rotation of a kinematic body
+
+    box1.body.needUpdate = true
   }
 }
 
