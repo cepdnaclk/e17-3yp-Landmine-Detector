ab32536cfc24bc124e6bbbca956dd89755e4dbd2
Akilax0 <killerilaksha@gmail.com>
2021-09-17 23:46:29 +0530

    api_init

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -3,14 +3,12 @@ import React, {useState, useEffect} from 'react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import { listSearches } from './graphql/queries';
 import { createSearch, deleteSearch } from './graphql/mutations';
-import logo from './logo.svg';
 import './App.css';
-import Amplify, {API} from 'aws-amplify';
+import Amplify, {API, graphqlOperation} from 'aws-amplify';
 import config from './aws-exports';
 
 
-const initialFormState = { name: '', description: '', startLoc: '' }
-
+const initialFormState = { name: '', description: '', startLoc: 0 }
 
 
 Amplify.configure(config);
@@ -25,22 +23,35 @@ function App() {
   }, []);
 
   async function fetchNotes() {
-    const apiData = await API.graphql({ query: listSearches });
+    try{
+    const apiData = await API.graphql(graphqlOperation(listSearches ));
     setNotes(apiData.data.listSearches.items);
+  } catch (err) {
+    console.log('error creating todo:', err)
+  }
   }
 
   async function createNote() {
+    try{
     if (!formData.name || !formData.description || !formData.startLoc) return;
     await API.graphql({ query: createSearch, variables: { input: formData } });
     setNotes([ ...notes, formData ]);
     setFormData(initialFormState);
+  } catch (err) {
+    console.log('error creating todo:', err)
+  }
   }
 
   async function deleteNote({ id }) {
+    try {
     const newNotesArray = notes.filter(note => note.id !== id);
     setNotes(newNotesArray);
     await API.graphql({ query: deleteSearch, variables: { input: { id } }});
+    } catch (err) {
+    console.log('error creating todo:', err)
   }
+  }
+
 
   return (
     <div className="App">
@@ -64,7 +75,7 @@ function App() {
       <div style={{marginBottom: 30}}>
         {
           notes.map(note => (
-            <div key={note.id || note.name}>
+            <div key={note.id || note.name }>
               <h2>{note.name}</h2>
               <p>{note.description}</p>
               <p>{note.startLoc}</p>
