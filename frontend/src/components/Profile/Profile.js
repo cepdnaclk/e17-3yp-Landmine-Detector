5e0af07bd39331044786d14177a3aa76158b2140
thisaramanohara <thisaramanohara@gmail.com>
2021-09-20 20:53:43 +0530

    Sidebar low level started

---
+++ frontend/src/components/Profile/Profile.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Profile.css'
+
+function Profile() {
+  return(
+    <div className='sidebar'>
+      <header>Landmine detector</header>
+      <ul>
+        <li>Profile</li>
+        <li>Profile</li>
+        <li>Profile</li>
+        <li>Profile</li>
+        <li>Profile</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Profile;
