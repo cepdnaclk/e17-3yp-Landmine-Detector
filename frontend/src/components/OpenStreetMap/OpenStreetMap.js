d14ea1f649caa69782bb6420736d03c6491f621a
thisaramanohara <thisaramanohara@gmail.com>
2021-09-25 01:03:17 +0530

    OpenStreetMap created v1

--- frontend/src/components/OpenStreetMap/OpenStreetMap.js
+++ frontend/src/components/OpenStreetMap/OpenStreetMap.js
@@ -1,17 +1,34 @@
+import { Marker } from "leaflet";
 import React from "react";
+import { geolocated } from "react-geolocated";
 import { Map, TileLayer } from 'react-leaflet'
 
 const DEFAULT_LANGITUDE = -123
 const DEFAULT_LATITUDE = 48
 
 function OpenStreetMap() {
+    const lan = DEFAULT_LANGITUDE
+    const lat = DEFAULT_LATITUDE
     return(
-        <Map center={[DEFAULT_LATITUDE,DEFAULT_LANGITUDE]}>
+        <Map center={[lan,lat]} zoom={12}>
             <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            
+
+        {
+            <Marker
+                position={[lan,lat]}
+            />
+        }
+
         </Map>
     )
 }
+
+export default geolocated({
+    positionOptions:{
+        enableHighAccuracy:false
+    },
+    userDecisionTimeout:10000
+})(OpenStreetMap);
