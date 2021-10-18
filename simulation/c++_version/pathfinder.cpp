901f28bf2230dfe2848b09b596f9b90d0d6ac928
Akilax0 <killerilaksha@gmail.com>
2021-10-18 17:30:54 +0530

    lambda trigger

--- simulation/c++_version/pathfinder.cpp
+++ simulation/c++_version/pathfinder.cpp
@@ -12,12 +12,19 @@ using namespace sf;
 int main(){
 	
 	int window_size = 500;
-	int sq_size = 10;
-	int search_area = 100;
+	int sq_size = 20;
+	int search_area = 20;
 	int sq_num = window_size/sq_size;
 	int area[sq_num][sq_num];
 
 	std::memset(area,0,sizeof(area));
+
+	for(int i=0;i<window_size;i+=sq_size){
+		for(int j=0;j<window_size;j+=sq_size){
+			if((i>search_area && window_size-search_area>i) && (j>search_area && window_size-search_area>j))
+				area[i][j] = 1;
+		}	
+	}
 
 	RenderWindow window(VideoMode(window_size,window_size), "PathFinder");
 	
@@ -31,7 +38,7 @@ int main(){
 	//robot 
 	CircleShape robot(5.f);
 	robot.setFillColor(Color::Green);
-	robot.setPosition(10,window_size-20);
+	robot.setPosition(10,window_size-10);
 
 	while(window.isOpen()){
 		Event event;
@@ -44,7 +51,7 @@ int main(){
 		
 		for(int i=0;i<window_size;i+=sq_size){
 			for(int j=0;j<window_size;j+=sq_size){
-				if((i>search_area && window_size-search_area>i) && (j>search_area && window_size-search_area>j))
+				if(area[i][j] == 1)
 					rect.setFillColor(Color::White);
 				else
 					rect.setFillColor(Color::Black);
