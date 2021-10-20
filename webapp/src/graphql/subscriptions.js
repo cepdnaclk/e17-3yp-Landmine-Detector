8dbe03d93fe95776292d6a80a23719facfbb1066
Akilax0 <killerilaksha@gmail.com>
2021-10-20 16:25:52 +0530

    lambda robot status

--- webapp/src/graphql/subscriptions.js
+++ webapp/src/graphql/subscriptions.js
@@ -2,8 +2,8 @@
 // this is an auto generated file. This will be overwritten
 
 export const onCreateSearch = /* GraphQL */ `
-  subscription OnCreateSearch {
-    onCreateSearch {
+  subscription OnCreateSearch($owner: String) {
+    onCreateSearch(owner: $owner) {
       id
       RobotID
       UserID
@@ -31,12 +31,13 @@ export const onCreateSearch = /* GraphQL */ `
       }
       createdAt
       updatedAt
+      owner
     }
   }
 `;
 export const onUpdateSearch = /* GraphQL */ `
-  subscription OnUpdateSearch {
-    onUpdateSearch {
+  subscription OnUpdateSearch($owner: String) {
+    onUpdateSearch(owner: $owner) {
       id
       RobotID
       UserID
@@ -62,41 +63,109 @@ export const onUpdateSearch = /* GraphQL */ `
         isObs
         isClear
       }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onDeleteSearch = /* GraphQL */ `
+  subscription OnDeleteSearch($owner: String) {
+    onDeleteSearch(owner: $owner) {
+      id
+      RobotID
+      UserID
+      name
+      description
+      searchLat
+      searchLon
+      startLot
+      startLon
+      LocationData {
+        Lat
+        Lon
+        Elev
+        isMine
+        isObs
+        isClear
+      }
+      PathData {
+        Lat
+        Lon
+        Elev
+        isMine
+        isObs
+        isClear
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onCreateRobot = /* GraphQL */ `
+  subscription OnCreateRobot {
+    onCreateRobot {
+      id
+      status
       createdAt
       updatedAt
     }
   }
 `;
-export const onDeleteSearch = /* GraphQL */ `
-  subscription OnDeleteSearch {
-    onDeleteSearch {
+export const onUpdateRobot = /* GraphQL */ `
+  subscription OnUpdateRobot {
+    onUpdateRobot {
       id
-      RobotID
-      UserID
-      name
-      description
-      searchLat
-      searchLon
-      startLot
-      startLon
-      LocationData {
-        Lat
-        Lon
-        Elev
-        isMine
-        isObs
-        isClear
-      }
-      PathData {
-        Lat
-        Lon
-        Elev
-        isMine
-        isObs
-        isClear
-      }
+      status
       createdAt
       updatedAt
+    }
+  }
+`;
+export const onDeleteRobot = /* GraphQL */ `
+  subscription OnDeleteRobot {
+    onDeleteRobot {
+      id
+      status
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser($owner: String) {
+    onCreateUser(owner: $owner) {
+      id
+      username
+      GroupID
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser($owner: String) {
+    onUpdateUser(owner: $owner) {
+      id
+      username
+      GroupID
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser($owner: String) {
+    onDeleteUser(owner: $owner) {
+      id
+      username
+      GroupID
+      createdAt
+      updatedAt
+      owner
     }
   }
 `;
