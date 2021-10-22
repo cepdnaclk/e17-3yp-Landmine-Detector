16ea268376eba5512c99d44dee88f019d4a0af8f
thisaramanohara <thisaramanohara@gmail.com>
2021-10-22 11:31:06 +0530

    Profile somewhat ok

--- webapp/src/components/Profile/Profile.js
+++ webapp/src/components/Profile/Profile.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getSearch, listSearches } from '../../graphql/queries';
+import { createSearch, updateSearch , deleteSearch } from '../../graphql/mutations';
+import Amplify, {API, graphqlOperation, Auth} from 'aws-amplify';
 
+
+
+const prof = {name: ''}
 
 function Tasks() {
-  return (
-      <h1>Profile</h1>    
+
+  
+
+
+  Auth.currentUserPoolUser()
+  .then(user=>{
+    // console.log(user);
+    prof.name = user.username;
+    // setFormData({ ...formData, 'id': searchID})
+
+    console.log(prof.name);
+  } )
+
+  console.log('heyyy'+prof.name);
+  return(
+    <div>
+      {console.log('hi'+prof.name)}
+      <p>{prof.name}</p>
+      {/* <p>Thisara Manohara</p> */}
+    </div>
   );
-}
+
+  }
+
 
 export default Tasks;
