48e9462904bd38eec7bba38ed5f752361067acad
VishvaNavanjana <vishvanavanjana@gmail.com>
2021-10-22 18:47:03 +0530

    start with profile page and some changes in UI

--- webapp/src/components/Profile/Profile.js
+++ webapp/src/components/Profile/Profile.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import Amplify, {API, graphqlOperation, Auth} from 'aws-amplify';
+
+var userID   = '';
+
+function UsereDetails(){
+  
+  Auth.currentUserPoolUser()
+        .then(user=>{
+          
+          // userID = user.userID
+          console.log(user)
+          userID = user.username 
+          console.log(userID)
+        })
+
+        return userID
+}
+
 
 
 function Tasks() {
-  return (
-      <h1>Profile</h1>    
+
+  return(
+
+    <div>
+      <button   onClick={UsereDetails}>User Details</button>
+      <h1>user ID:{userID}</h1>
+      <p>{userID}</p>
+      
+    </div>
+
   );
+  
+  
 }
 
 export default Tasks;
