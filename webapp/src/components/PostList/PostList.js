9f1b28b7480ad0ca2c625198df78632c26b7b270
VishvaNavanjana <vishvanavanjana@gmail.com>
2021-10-23 14:05:26 +0530

    Created a HTTP API to get robotable

---
+++ webapp/src/components/PostList/PostList.js
@@ -0,0 +1,44 @@
+import React,{Component} from "react";
+import axios from 'axios'
+
+class PostList extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            robots:[]
+        }
+    }
+
+    componentDidMount(){
+        axios.get("https://6zx50pbvqb.execute-api.us-east-1.amazonaws.com/items")
+        .then(response => {
+            console.log(response)
+            this.setState({robots: response.data})
+            console.log({ldr: response.data})
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
+
+
+    render(){
+        const { robots } = this.state
+        return(
+            <div>
+                List Of Robots
+                {robots.items}
+                
+                {
+                    robots.length?
+                    robots.map(robot => <div key={robot.id}>{robot.status}</div>) :
+                    null
+                }
+            </div>
+        )
+    }
+}
+
+export default PostList
