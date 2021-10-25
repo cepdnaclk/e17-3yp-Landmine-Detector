08d6d3f7ee282f91780b9bb9088c48dd75b5304c
Akilax0 <killerilaksha@gmail.com>
2021-10-25 18:33:14 +0530

    Testing

--- website/src/pages/Testing/Testing.js
+++ website/src/pages/Testing/Testing.js
@@ -2,32 +2,24 @@ import './Testing.css'
 
 function Testing(){
     return(
-        <div className="each-slide-sa">
+     <div className="slide-show">
             <h1 style={{color:"#43dea5", textAlign:'center' }}>Testing</h1>  
-            <br></br> 
+            <br></br>    
 
-            
-                <h1 className='sub-topic-hd'>User Interface & login Testing </h1>
-
-
-                <h1 className='sub-topic-hd'>Backend Testing</h1>
-                            <div className="left-sa">
-                                <img alt="" src={process.env.PUBLIC_URL + '/images/postman_logo.png'} style={{ display: 'block', marginRight:'auto',width:'40%',opacity:'70%'}}/> 
-                                <ul  className='ha-text'>
-                                    <li>GraphQL server is setup using AWS AppSync</li> <br></br>
-                                    <li>Postman for backend testing </li>
-                                        <ul>
-                                            <li>Send requests to read/write to our database</li>
-                                            <li>Testing Lambda functions </li>
-                                            <li>Test publishing MQTT message</li>
-                                        </ul>
-                                </ul>
+                    <div className="each-slide">
+                    <h1 className='sub-topic-ha'>API</h1>
+                        <div className="left-sa">
+                            <img alt="" src={process.env.PUBLIC_URL + '/images/connection.png'} style={{ display: 'block', marginRight:'auto',width:'90%',opacity:'70%'}}/> 
+                            <div  className='hd-text'>
+                                <p>The robot has a main I2C bus connecting two masters and sensors as slaves. Also an atmel chip is used as a 
+                                    slave for controlling motors and interrupt generator for the metal detector. One aster being the ESP32 handles the calculations
+                                    while the second master the atmel chip is responsible for communiation and data buffering. The data would be stored in an SD card 
+                                    using the SPI conncetion protocol. Communication happens through the SIM900A GSM module using the UART protocol.
+                                </p>
                             </div>
-                    
-
-
-            </div>
-
+                        </div>
+                    </div>
+           </div>
 
     )
 }
