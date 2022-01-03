3d4ef7286219ba8bb34e1cb1d0290504d664c680
akilax0 <killerilaksha@gmail.com>
2022-01-03 19:48:30 +0530

    new website introduction

--- new_website/website/src/components/Cards.js
+++ new_website/website/src/components/Cards.js
@@ -6,6 +6,7 @@ import CardItem from './CardItem';
 
 function Cards(){
 	return (
+		<>
 		<div className='cards'>
 			<h1> Hardware Architecture </h1>
 			<div className="cards__container">
@@ -27,6 +28,53 @@ function Cards(){
 				</div>
 			</div>
 		</div>
+
+
+		<div className='cards'>
+			<h1> Software Architecture </h1>
+			<div className="cards__container">
+				<div className="cards_wrapper">
+					<ul className="cards__items">
+					<CardItem
+			src="images/Hardware.png"
+			text="Hardware architecture"
+			label="Design"
+			path="/hardware"
+		/>	
+					<CardItem
+			src="images/Cloud_Security.png"
+			text="Software architecture"
+			label="Design"
+			path="/hardware"
+		/>	
+					</ul>
+				</div>
+			</div>
+		</div>
+
+
+		<div className='cards'>
+			<h1> Autonmous Navigation </h1>
+			<div className="cards__container">
+				<div className="cards_wrapper">
+					<ul className="cards__items">
+					<CardItem
+			src="images/Hardware.png"
+			text="Hardware architecture"
+			label="Design"
+			path="/hardware"
+		/>	
+					<CardItem
+			src="images/Cloud_Security.png"
+			text="Software architecture"
+			label="Design"
+			path="/hardware"
+		/>	
+					</ul>
+				</div>
+			</div>
+		</div>
+		</>
 	)
 }
 
---
+++ new_website/website/src/components/Introduction.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import './Introduction.css'
+
+
+// Only loads the YouTube player
+
+
+function Introduction() {
+    return(
+        <div className="text-block">
+            <h1 className='main-topic'>Introduction</h1>
+            {/* <div className="intro-vid"><ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' /></div>
+             */}
+             {/* <video autoPlay loop id='video'>
+                 <source src={vid} type='video/mp4'></source>
+            </video>  */}
+
+
+            <h1 className='sub-topic'>About</h1>
+            <p className='text'>Landmines are prominent weapons designed to destroy or 
+            disable enemy targets which detonate by the movement of its target, pressure, 
+            sound, magnetism and vibration. More than a hundred million land mines are scattered 
+            around the world which remain hazardous for years even after conflict termination
+             posing a significant threat to civilians and the economy. More Effective and 
+             sophisticated tools to detect, locate and deactivate landmines are urgently needed
+              to make lands safer for humans to use.</p>
+
+            <br></br>
+            <h1 className='sub-topic'>Problem</h1>
+            <p className='text'>Currently available demining methods which include humanitarian 
+            and mechanical demining utilize manual prodders or metal detectors operated by humans
+             and mine clearing machines respectively to detect the location of mines. This conventional
+              methods are labor intensive, expensive, time consuming and possess high risk for the 
+              humans and machines involved in the process.</p>
+
+            <br></br>
+            <h1 className='sub-topic'>Solution</h1>
+            <p className='text'>We introduce 'The Landmine Detection Robot' that accurately detects mines 
+            through sensors and send the GPS coordinates of the specific locations where mines are 
+            located to the server where a map of the mine field is updated. The deployment of multiple
+            Mine Detection Robots to the mine field facilitates finding a safe pathway through the
+             mine fields safely and efficiently without any human involvement making the challenging 
+             process of demining easier than ever.</p>
+        </div>
+    )
+}
+
+
+export default Introduction;
