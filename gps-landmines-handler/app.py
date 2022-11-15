a77d3818c6a6edb7242b8f6b06082619cb559800
thisaramanohara <thisaramanohara@gmail.com>
2022-11-15 16:20:49 +0530

    Lat and lon ok

--- gps-landmines-handler/app.py
+++ gps-landmines-handler/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_mqtt import Mqtt
+import json
 
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ app.config['MQTT_KEEPALIVE'] = 5  # Set KeepAlive time in seconds
 # app.config['MQTT_TLS_ENABLED'] = False  # If your server supports TLS, set it True
 
 topic = '/3yp/flask/test'
+landmines = []
 
 mqtt_client = Mqtt(app)
 
@@ -29,62 +31,19 @@ def handle_mqtt_message(client, userdata, message):
        topic=message.topic,
        payload=message.payload.decode()
     )
-   print('Received message on topic: {topic} with payload: {payload}'.format(**data))
+   # print('Received message on topic: {topic} with payload: {payload}'.format(**data))
+   data = json.loads(data['payload'])
+
+   newLandmine = [data['lat'], data['lon']]
+   landmines.append(newLandmine)
+   print(landmines)
+
+@app.route('/send-landmines', methods=['POST'])
+def sendLandmines():
+    request_data = request.get_json()
 
 
-@app.route('/', methods=['GET'])
-def home():
-   return "Hello thisara"
+    return jsonify({'status': "Coorinates added"})
 
 if __name__ == '__main__':
    app.run(host='127.0.0.1', port=5000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-# from flask import Flask
-#
-# import json
-# import paho.mqtt.client as mqtt
-#
-# client = mqtt.Client("gps-landmines handler")
-#
-# # subscribing topics
-# coorinatesTopic = 'topics/coordinates'
-# landminesTopic = 'topic/landmines'
-#
-# landmines = []
-#
-# def on_message_for_landmine(client, userdata, message):
-#     data = json.loads(message.payload)
-#     values = list(data.values())
-#     print(values)
-#
-# client.message_callback_add(landminesTopic, on_message_for_landmine)
-# client.connect("mqtt.eclipseprojects.io", port=1883)
-# client.subscribe([(landminesTopic, 0)])
-#
-#
-# app = Flask(__name__)
-#
-# @app.route('/')
-# def home():
-#     return "Testing"
-#
-# client.loop_forever()
