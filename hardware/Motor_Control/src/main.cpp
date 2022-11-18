983fdc7410e871cc179dfe40cc5ecd7a769cccbb
Akilax0 <killerilaksha@gmail.com>
2022-11-18 11:24:53 +0530

    motor controls

--- hardware/Motor_Control/src/main.cpp
+++ hardware/Motor_Control/src/main.cpp
@@ -43,10 +43,10 @@ void setup() {
   // sets the pins as outputs:
   pinMode(motor1Pin1, OUTPUT);
   pinMode(motor1Pin2, OUTPUT);
-  pinMode(enable1Pin, OUTPUT);
+  // pinMode(enable1Pin, OUTPUT);
   pinMode(motor2Pin1, OUTPUT);
   pinMode(motor2Pin2, OUTPUT);
-  pinMode(enable2Pin, OUTPUT);
+  // pinMode(enable2Pin, OUTPUT);
   
   // configure LED PWM functionalitites
   ledcSetup(pwmChannel, freq, resolution);
@@ -65,15 +65,15 @@ void setup() {
                     0);          /* pin task to core 0 */                  
   delay(500); 
         //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
-  xTaskCreatePinnedToCore(
-                    Task2code,   /* Task function. */
-                    "Task2",     /* name of task. */
-                    10000,       /* Stack size of task */
-                    NULL,        /* parameter of the task */
-                    1,           /* priority of the task */
-                    &Task2,      /* Task handle to keep track of created task */
-                    0);          /* pin task to core 0 */                  
-  delay(500); 
+//   xTaskCreatePinnedToCore(
+//                     Task2code,   /* Task function. */
+//                     "Task2",     /* name of task. */
+//                     10000,       /* Stack size of task */
+//                     NULL,        /* parameter of the task */
+//                     1,           /* priority of the task */
+//                     &Task2,      /* Task handle to keep track of created task */
+//                     0);          /* pin task to core 0 */                  
+//   delay(500); 
 }
 
 
@@ -83,11 +83,66 @@ void Task1code( void * pvParameters ){
   Serial.println(xPortGetCoreID());
 
   for(;;){
-    if (Serial.available()) {
-      SerialBT.write(Serial.read());
-    }
+    // if (Serial.available()) {
+    //   SerialBT.write(Serial.read());
+    // }
     if (SerialBT.available()) {
-      Serial.write(SerialBT.read());
+      char o = SerialBT.read();
+      Serial.write(o);
+
+      if(o=='F'){
+        Serial.println("Moving Forward");
+        digitalWrite(motor1Pin1, LOW);
+        digitalWrite(motor1Pin2, HIGH); 
+
+        digitalWrite(motor2Pin1, LOW);
+        digitalWrite(motor2Pin2, HIGH); 
+        delay(1000);
+      }
+
+      if(o=='S'){
+        Serial.println("Motor stopped");
+        digitalWrite(motor1Pin1, LOW);
+        digitalWrite(motor1Pin2, LOW);
+
+        digitalWrite(motor2Pin1, LOW);
+        digitalWrite(motor2Pin2, LOW);
+        delay(1000);
+      }
+
+      if(o=='L'){
+        Serial.println("Left Turn");
+        digitalWrite(motor1Pin1, LOW);
+        digitalWrite(motor1Pin2, HIGH);
+
+        digitalWrite(motor2Pin1, HIGH);
+        digitalWrite(motor2Pin2, LOW);
+        delay(1000);
+      }
+
+      if(o=='R'){
+        Serial.println("Right Turn");
+        digitalWrite(motor1Pin1, HIGH);
+        digitalWrite(motor1Pin2, LOW);
+
+        digitalWrite(motor2Pin1, LOW);
+        digitalWrite(motor2Pin2, HIGH);
+        delay(1000);
+      }
+
+      if(o=='B'){
+        Serial.println("Reverse");
+        digitalWrite(motor1Pin1, HIGH);
+        digitalWrite(motor1Pin2, LOW);
+
+        digitalWrite(motor2Pin1, HIGH);
+        digitalWrite(motor2Pin2, LOW);
+        delay(1000);
+      }
+
+
+
+      
     }
 
     delay(50);
@@ -100,58 +155,58 @@ void Task2code( void * pvParameters ){
   Serial.println(xPortGetCoreID());
 
   for(;;){
-  // Move the DC motor forward at maximum speed
-  Serial.println("Moving Forward");
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, HIGH); 
+  // // Move the DC motor forward at maximum speed
+  // Serial.println("Moving Forward");
+  // digitalWrite(motor1Pin1, LOW);
+  // digitalWrite(motor1Pin2, HIGH); 
 
-  digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, HIGH); 
-  delay(2000);
+  // digitalWrite(motor2Pin1, LOW);
+  // digitalWrite(motor2Pin2, HIGH); 
+  // delay(2000);
 
-  // Stop the DC motor
-  Serial.println("Motor stopped");
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, LOW);
+  // // Stop the DC motor
+  // Serial.println("Motor stopped");
+  // digitalWrite(motor1Pin1, LOW);
+  // digitalWrite(motor1Pin2, LOW);
 
-  digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, LOW);
-  delay(1000);
+  // digitalWrite(motor2Pin1, LOW);
+  // digitalWrite(motor2Pin2, LOW);
+  // delay(1000);
 
-  // Move DC motor backwards at maximum speed
-  Serial.println("Moving Backwards");
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW); 
+  // // Move DC motor backwards at maximum speed
+  // Serial.println("Moving Backwards");
+  // digitalWrite(motor1Pin1, HIGH);
+  // digitalWrite(motor1Pin2, LOW); 
 
-  digitalWrite(motor2Pin1, HIGH);
-  digitalWrite(motor2Pin2, LOW); 
-  delay(2000);
+  // digitalWrite(motor2Pin1, HIGH);
+  // digitalWrite(motor2Pin2, LOW); 
+  // delay(2000);
 
-  // Stop the DC motor
-  Serial.println("Motor stopped");
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, LOW);
+  // // Stop the DC motor
+  // Serial.println("Motor stopped");
+  // digitalWrite(motor1Pin1, LOW);
+  // digitalWrite(motor1Pin2, LOW);
 
-  digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, LOW);
-  delay(1000);
+  // digitalWrite(motor2Pin1, LOW);
+  // digitalWrite(motor2Pin2, LOW);
+  // delay(1000);
 
-  // Move DC motor forward with increasing speed
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
+  // // Move DC motor forward with increasing speed
+  // digitalWrite(motor1Pin1, HIGH);
+  // digitalWrite(motor1Pin2, LOW);
 
-  digitalWrite(motor2Pin1, HIGH);
-  digitalWrite(motor2Pin2, LOW);
+  // digitalWrite(motor2Pin1, HIGH);
+  // digitalWrite(motor2Pin2, LOW);
 
 
-  while (dutyCycle <= 255){
-    ledcWrite(pwmChannel, dutyCycle);   
-    Serial.print("Forward with duty cycle: ");
-    Serial.println(dutyCycle);
-    dutyCycle = dutyCycle + 5;
-    delay(500);
-  }
-  dutyCycle = 200;
+  // while (dutyCycle <= 255){
+  //   ledcWrite(pwmChannel, dutyCycle);   
+  //   Serial.print("Forward with duty cycle: ");
+  //   Serial.println(dutyCycle);
+  //   dutyCycle = dutyCycle + 5;
+  //   delay(500);
+  // }
+  // dutyCycle = 200;
   } 
 }
 
