fd98c1c8191ce5e3b964416ff5144537127bca36
thisaramanohara <thisaramanohara@gmail.com>
2022-11-24 22:37:59 +0530

    Unit testing added

--- robot-controller/app.js
+++ robot-controller/app.js
@@ -1,4 +1,13 @@
 const express = require('express');
+
+// mqtt things
+const mqtt = require('mqtt')
+var client = mqtt.connect('https://mqtt.eclipseprojects.io/:1883');
+// var client = mqtt.connect('https://broker.hivemq.com:1883');
+const robotControlTopicLDR001 = 'robot-control/ldr001'
+
+
+
 const app = express()
 const PORT = 5000;
 
@@ -8,11 +17,13 @@ app.post('/robot-control', (req, res)=>{
 
     if (req.body.status == 1) {
         // send mqtt msg
+        client.publish(robotControlTopicLDR001, "Turn ON")
         res.status(200).send({
             command: `turining on the robot ${req.body.id}`
         });
     } else if (req.body.status == 0) {
         // send mqtt msg
+        client.publish(robotControlTopicLDR001, "Turn OFF")
         res.status(200).send({
             command: `turining off the robot ${req.body.id}`
         });
--- website/src/pages/Testing/Testing.js
+++ website/src/pages/Testing/Testing.js
@@ -77,6 +77,40 @@ function Testing(){
                     
 
            </div>
+
+           <h2 style={{color:"#43dea5", textAlign:'left' }}>Unit Testing in PlatformIO</h2> 
+
+{/* <h1 className='sub-topic-ha'>Manual Controller</h1> */}
+
+<div className="left-sa">
+
+        <div className="left-sa">
+                            <img alt="" src={process.env.PUBLIC_URL + '/images/unit.png'} style={{ display: 'block', marginRight:'auto',width:'90%',opacity:'70%'}}/> 
+                        </div>
+
+            
+            {/* <img alt="" src={process.env.PUBLIC_URL + '/images/aws_lambda.png'} style={{ display: 'block', marginRight:'auto',width:'90%',opacity:'70%'}}/>  */}
+
+        
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         </div>
         </div>
 
